f574e6c10e269d5e658491631c118f48921a11e8
root <root@bastion.2954.example.opentlc.com>
2018-08-03 09:28:29 +0000

    Permissions for git and prerequisites checking

--- install.sh
+++ install.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-export GUID=`hostname|awk -F. '{print $2}'`
-echo -- Your GUID is $GUID --
+while getopts g:i:e: option
+do
+case "${option}"
+in
+g) GUID=${OPTARG};;
+i) INTERNAL=${OPTARG};;
+e) EXTERNAL=${OPTARG};;
+esac
+done
 
-echo "-- Clonging git repo --"
-# git clone https://github.com/gszerlomski/os-adv-depl-hw.git
+
+#export GUID=`hostname|awk -F. '{print $2}'`
+echo -- GUID = $GUID --
+echo -- Internal domain = $INTERNAL --
+echo -- External domain = $EXTERNAL -- 
+
+
+
+echo  "-- Preparing hosts file --"
+cat hosts.template | sed -e "s:{GUID}:$GUID:g;s:{DOMAIN_INTERNAL}:$INTERNAL:g;s:{DOMAIN_EXTERNAL}:$EXTERNAL:g;" > hosts
+
+echo -- Installing atomic packages --
+#yum -y install atomic-openshift-utils atomic-openshift-clients
+
+
+echo -- Checking Openshift Prerequisites --
+if ansible-playbook -f 20 -i ./hosts /usr/share/ansible/openshift-ansible/playbooks/prerequisites.yml ; then
+    echo -- Prerequisites successful. Installing Openshift --
+
+
+
+else
+    echo -- Prerequisites failed --
+fi
